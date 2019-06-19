ad22ca9f1cff66368d7ac81bea3e9608837334b8
HenryFBP <HenryFBP@gmail.com>
2019-06-18 20:33:57 -0500

    don't use tuple as it is immutable

--- challenge3.py
+++ challenge3.py
@@ -3,21 +3,24 @@ import unittest
 
 def followDirections(directions):
     """
-    Given a string comprised of four characters, '^', 'v', '<', and '>', return an (x,y) tuple that
+    Given a string comprised of four characters, '^', 'v', '<', and '>', return an [x, y] list that
     represents following those directions on a taxicab-distance grid.
 
-    You start at (0, 0) and the coordinates are (x, y).
+    You start at [0, 0] and the coordinates are [x, y].
 
-    :example: followDirections("") -> (0, 0)
-    :example: followDirections("v") -> (0, -1)
-    :example: followDirections(">>>^") -> (3, 1)
-    :example: followDirections(">v<^") -> (0, 0)
+    :example: followDirections("") -> [0, 0]
+    :example: followDirections("v") -> [0, -1]
+    :example: followDirections(">>>^") -> [3, 1]
+    :example: followDirections(">v<^") -> [0, 0]
 
     """
     raise NotImplementedError("Finish me! Delete this line and code.")
 
 
-class TestCats(unittest.TestCase):
+class TestFollowDirections(unittest.TestCase):
 
     def test_simple(self):
-        self.assertEqual(followDirections(""), 0)
+        self.assertEqual(followDirections(""), [0, 0])
+        self.assertEqual(followDirections("^^^"), [0, 3])
+        self.assertEqual(followDirections("^^^vvv"), [0, 0])
+        self.assertEqual(followDirections("^^v>^vv<<<v"), [-2, -1])
